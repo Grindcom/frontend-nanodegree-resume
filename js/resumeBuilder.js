983efcadecdea4cfac154b270d19ca3aa277fe8d
Greg Ford <ford@grindcom.ca>
2016-09-29 12:38:39 -0700

    Add replace example.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ // Access a tag id from JavaScript
  $("#main").append("Greg Ford, B.Sc.");
+ // How to use variables and print to console
  var firstName = "Greg";
  var lastName = "Ford";
  console.log("My name is " + firstName);
+ // Using replace
+ var oldEmail = "ford@grindcom.ca";
+ var newEmail = oldEmail.replace("grindcom","facet-it");
+ console.log("My first email was "+oldEmail);
+ console.log("But now I use "+newEmail+" for business");
