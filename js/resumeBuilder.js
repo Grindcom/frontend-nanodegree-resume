b301d3c40e8aa95b9ce0710d77823e37e8648d7a
Greg Ford <ford@grindcom.ca>
2016-09-29 12:57:09 -0700

    Add name and role to header.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,3 +12,15 @@ This is empty on purpose! Your code to build the resume will go here.
  var newEmail = oldEmail.replace("grindcom","facet-it");
  console.log("My first email was "+oldEmail);
  console.log("But now I use "+newEmail+" for business");
+ //
+ // Set up Header
+ //
+ // Set up name and role variables
+var name = firstName + " " + lastName + " B.sc.";
+var formattedName = HTMLheaderName.replace("%data%",name);
+var role = "Front-end Developer";
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+// Add to header
+//  Use prepend to put it at the start of the header area
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
