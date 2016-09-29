69b9c974ae71aba33b4ea98a7da9a8456a8093fc
Greg Ford <ford@grindcom.ca>
2016-09-29 12:27:10 -0700

    Using keyword var to assign variables and print do console.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,6 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  $("#main").append("Greg Ford, B.Sc.");
+ var firstName = "Greg";
+ var lastName = "Ford";
+ console.log("My name is " + firstName);
